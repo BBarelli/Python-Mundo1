6a9b8450a2810063feb8231ae98bf1d5c221d4a4
BBarelli <breno.barelli4@gmail.com>
2025-05-06 14:11:17 -0300

    Lixo

--- ex001.py
+++
@@ -1,2 +0,0 @@
-#Exercício Python 1: Crie um programa que escreva 'Olá Mundo'
-print('Olá Mundo!')
--- ex002.py
+++
@@ -1,3 +0,0 @@
-# Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
-nome = input ('Digite seu nome: ')
-print ('Olá {}, Seja Bem-Vindo ao curso de Python.'.format(nome))
