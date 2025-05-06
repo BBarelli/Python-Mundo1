55bf0feb39a9fc08ee76afcd7dc0c6f186e6ede8
BBarelli <breno.barelli4@gmail.com>
2025-05-06 11:18:05 -0300

    Exercício Python 10

---
+++ ex010.py
@@ -0,0 +1,4 @@
+#Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+real = float(input('Qual o saldo da sua carteira: ')
+dolar = real / 3.27
+print('O seu saldo é {} R&, com esse valor é possível comprar em dolar: {}US'.format(real, dolar))
