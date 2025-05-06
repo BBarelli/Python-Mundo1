1952620629459af95a26ca5b1a4607f51369164c
BBarelli <breno.barelli4@gmail.com>
2025-05-06 17:24:52 -0300

    Exercício Python - Curso em Vídeo

--- ex013.py
+++ ex013.py
@@ -1,4 +1,4 @@
 #Exercício Python 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
 n = float (input('Digite seu salário: '))
-salario = n * 1.15
+salario = n * 1.15 # ou n * 0,15
 print('Parabéns você recebeu um aumento! seu novo salário é: {:.2f} '.format(salario))
