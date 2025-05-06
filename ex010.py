c8f83d3cd165e3e951f8e8a5cd6a58b04f1f20c6
BBarelli <breno.barelli4@gmail.com>
2025-05-06 13:36:04 -0300

    Lixo

--- ex010.py
+++
@@ -1,6 +0,0 @@
-#Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
-
-real = float(input('Qual o saldo da sua carteira? R$ '))
-dolar = real / 3.27
-print('O seu saldo é R$ {:.2F}, com esse valor é possível comprar em dolar: US {:.2F}'.format(real, dolar))
-
--- ex013.py
+++
@@ -1,4 +0,0 @@
-#Exercício Python 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-salario = float(input('Digite o valor do seu salário: R$ '))
-novo = salario + (salario * 15 / 100)
-print('Seu sálario no valor de R$ {:.2f}, com o aumento de 15% passa a ser no valor de R${:.2f}'.format(salario, novo))
