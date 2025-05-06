b8575a350ed800b7867b011ccbba8d257d0fb160
BBarelli <breno.barelli4@gmail.com>
2025-05-06 15:13:05 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex007.py
@@ -0,0 +1,5 @@
+#Exercício Python 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+n1 = float(input('Digite sua nota1: '))
+n2 = float(input('Digite sua nota2: '))
+media = (n1 + n2) / 2
+print('Sua média da etapa é: {}'.format(media))
