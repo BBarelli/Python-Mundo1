8bfe3a872e1ab9c10f7eb993059624a98c5d9b19
BBarelli <breno.barelli4@gmail.com>
2025-05-06 17:23:55 -0300

    Exercício Python - Curso em Vídeo

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
 #Exercício Python 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 n = float (input('Digite o valor do produto: '))
-preco = n * 0.95
+preco = n * 0.95 # se preferir ver o valor do desconto n * 0,05
 print('Valor do produto com desconto {}R$'.format(preco))
