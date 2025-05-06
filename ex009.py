185b64c18c5981ccdb421688154f4c6c5094789b
BBarelli <breno.barelli4@gmail.com>
2025-05-06 15:34:47 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex009.py
@@ -0,0 +1,13 @@
+#Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+n = int(input('Digite um número para sua tabuada: '))
+print('{} x {} é: {}'.format(n, 1, n*1))
+print('{} x {} é: {}'.format(n, 2, n*2))
+print('{} x {} é: {}'.format(n, 3, n*3))
+print('{} x {} é: {}'.format(n, 4, n*4))
+print('{} x {} é: {}'.format(n, 5, n*5))
+print('{} x {} é: {}'.format(n, 6, n*6))
+print('{} x {} é: {}'.format(n, 7, n*7))
+print('{} x {} é: {}'.format(n, 8, n*8))
+print('{} x {} é: {}'.format(n, 9, n*9))
+print('{} x {} é: {}'.format(n, 10, n*10))
+
