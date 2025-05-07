03bd66a2c5983a70c36a39fe89760e9de36c8b84
BBarelli <breno.barelli4@gmail.com>
2025-05-07 19:17:17 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex017.py
@@ -0,0 +1,3 @@
+# Exercício Python 17: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triagulo retangulo, calcule e mostre o comprimento da hipotenusa
+import math
+n = float (input('Digite o comprimento do cateto: '))
