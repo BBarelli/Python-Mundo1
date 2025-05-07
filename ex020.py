26d3e1d9214731979615308efdfd2f8814fdd3b0
BBarelli <breno.barelli4@gmail.com>
2025-05-07 20:00:37 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex020.py
@@ -0,0 +1,1 @@
+# Exercício Python 20: O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um progrma que leia nome dos quadros alunos e mostre a ordem sorteada
