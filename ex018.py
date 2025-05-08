fb802c650893c80bec948a3daa7804df76054f52
BBarelli <breno.barelli4@gmail.com>
2025-05-08 19:30:42 -0300

    Exercício Python - Curso em Vídeo

--- ex018.py
+++ ex018.py
@@ -1,5 +1,17 @@
 # Exercício Python 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo
 import math
-angulo = int (input('Digite o valor do angulo: '))
 #A maioria das funções trigonométricas em Python (e em muitas outras linguagens) trabalha com radianos, não com graus. Isso inclui as funções math.sin(), math.cos(), math.tan() e outras.
+angulo = int (input('Digite o valor do angulo: '))
+
 # A função math.radians() faz exatamente essa conversão.
+angulo_radianos = math.radians(angulo)
+
+#criando os parametros que faram o calculo
+seno = math.sin(angulo_radianos)
+cosseno = math.cos(angulo_radianos)
+tangente = math.tan(angulo_radianos)
+
+#Exibir
+print('O valor de seno é: {:.2f}'.format(seno))
+print('O valor de cosseno é: {:.2f}'.format(cosseno))
+print('O valor da tangente é: {:.2f}'.format(tangente))
