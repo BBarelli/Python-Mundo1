c5a1a7f5045da8d3c5c4e03b4d5267e6835e4386
BBarelli <breno.barelli4@gmail.com>
2025-05-08 09:18:45 -0300

    Exercício Python - Curso em Vídeo

--- ex017.py
+++ ex017.py
@@ -1,3 +1,8 @@
 # Exercício Python 17: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triagulo retangulo, calcule e mostre o comprimento da hipotenusa
+# Utilizar o teorema de pitágoras
 import math
-n = float (input('Digite o comprimento do cateto: '))
+co = int (input('Digite o comprimento do cateto oposto: '))
+ca = int (input('Digite o comprimento do cateto adjacente: '))
+hipotenusa = math.hypot(co, ca)
+print('O calculo entre o cateto oposto: {} e o cateto adjacente: {} é equivalente a hipotenusa: {}'.format(co, ca, hipotenusa))
+
--- ex018.py
+++ ex018.py
@@ -1,1 +1,5 @@
 # Exercício Python 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo
+import math
+angulo = int (input('Digite o valor do angulo: '))
+#A maioria das funções trigonométricas em Python (e em muitas outras linguagens) trabalha com radianos, não com graus. Isso inclui as funções math.sin(), math.cos(), math.tan() e outras.
+# A função math.radians() faz exatamente essa conversão.
