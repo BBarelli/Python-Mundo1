49ba1bb56448edcc5b686c6514af2123dcf486df
BBarelli <breno.barelli4@gmail.com>
2025-05-08 19:49:07 -0300

    Exercício Python - Curso em Vídeo

--- ex019.py
+++ ex019.py
@@ -1,1 +1,9 @@
 # Exercício Python 19: Um professor que sortear um dos seus quatros alunos para apagar o quadro. Faça um progrma que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
+import random
+
+a1 = input ('Primeiro aluno: ')
+a2 = input ('Segundo aluno: ')
+a3 = input ('Terceiro aluno: ')
+a4 = input ('Quarto aluno: ')
+nomes = a1, a2, a3, a4
+print(random.choice(nomes))
