aa6b23a1054f180b68d765c1b178c7976bfaff98
BBarelli <breno.barelli4@gmail.com>
2025-05-09 10:56:58 -0300

    Exercício Python - Curso em Vídeo

--- ex016.py
+++ ex016.py
@@ -1,5 +1,4 @@
 # Exercício Python 16:Crie um programa que leia um número Real qualquer pelo teclado e mostre sua porção inteira
 import math
 n = float (input('Digite um número: '))
-arred = math.trunc(n)
-print('O número digitado {}, tem a parte inteira: {} '.format(n, arred))
+print('O número digitado {}, tem a parte inteira: {} '.format(n, math.trunc(n)))
