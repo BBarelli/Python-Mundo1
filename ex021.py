c07fe26df9d58091b46f52e07681149a16a78db4
Breno Barelli <breno.barelli4@gmail.com>
2025-05-19 00:17:58 -0300

    Exercício Python - Curso em Vídeo

--- ex021.py
+++ ex021.py
@@ -1,1 +1,10 @@
 # Exercício Python 21: Faça um programa em Python que abre e reproduza o áudio de um arquivo MP3
+import pygame
+import os
+
+pygame.init()
+pygame.mixer.music.load('C:/Users/barel/OneDrive/Área de Trabalho/CURSOS/CursoEmVideo/PYTHON/PhytonExercicios/PythonExercicios/PythonExercicios/ex021.mp3')
+pygame.mixer.music.play()
+input("Pressione Enter para parar...")
+pygame.mixer.music.stop()
+
