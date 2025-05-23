6c8ca533186571613858e680b15a88b22cfde227
Breno Barelli <breno.barelli4@gmail.com>
2025-05-22 23:43:39 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex022.py
@@ -0,0 +1,14 @@
+# Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
+nome = input('Digite seu nome completo: ')
+
+print('Todas as letras Maiusculas') 
+print(nome.upper())
+
+print('Todas as letra minusculas')
+print(nome.lower())
+
+print('Quantas letras ao todo')
+print(len(nome))
+
+#Quantas letra tem o primeiro nome
+#print(nome[0:])
