5aaf57e4cc574c2c4383619901842b6780fd2146
Breno Barelli <breno.barelli4@gmail.com>
2025-05-23 00:35:22 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex024.py
@@ -0,0 +1,3 @@
+# Exercício Python 24: Crie um program que leia o nome de uma cidade e diga se ela começa ou não com o nome 'SANTO'
+cidade = input('Digite o nome da sua cidade: ')
+print('SANTO' in cidade)
