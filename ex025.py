cd75b7b5538a273be6ef4e3ee1f9c14338457228
Breno Barelli <breno.barelli4@gmail.com>
2025-05-23 00:53:42 -0300

    Exercício Python - Curso em Vídeo

--- ex025.py
+++ ex025.py
@@ -1,4 +1,4 @@
 # Exercício Python 25: Crie um programa que leia o nome de uma pessoa e diga se tem SILVA no nome
 nome = input('Digite seu nome: ')
 
-print(nome.find('SILVA'))
+print(nome.in fome)
