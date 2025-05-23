ee86187e043ae7598089f5e3ac7303e8b3f07c6a
Breno Barelli <breno.barelli4@gmail.com>
2025-05-23 00:45:54 -0300

    Exercício Python - Curso em Vídeo

--- ex023.py
+++ ex023.py
@@ -1,7 +1,7 @@
 # Exercício Python 23: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados
-numero = int(input('Digite seu número para analise: '))
+numero = (input('Digite seu número para analise: '))
 
-print ('Unidade :'), numero[1]
-#print('Dezena: '.numero[1])
-#print('Centena: '.numero[2]) 
-#print('Milhar: '.numero[3] )
+print ('Unidade:', numero[0])
+print('Dezena: ', numero[1])
+print('Centena: ', numero[2]) 
+print('Milhar: ', numero[3])
