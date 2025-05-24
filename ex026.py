627bc4c5bda81e7d31fce817dec9923e8165b193
Breno Barelli <breno.barelli4@gmail.com>
2025-05-24 20:48:08 -0300

    Exercício Python - Curso em Vídeo

--- ex026.py
+++ ex026.py
@@ -4,3 +4,11 @@
 #3.Em que posição ela aparece a última vez
 
 frase = input('Digite uma frase: ')
+print('Quantas vezes a letra (a) aparece: ') 
+frase.count('a')
+
+print('Em que posição ela aparece a primeira vez:')
+frase.count('a', 0:)
+
+print('Em que posição ela aparece a última vez')
+frase.count('a': :)
