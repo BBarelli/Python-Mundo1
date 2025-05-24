0d3727187f29cf13cc3e3bdb525fcaaef2c985a5
Breno Barelli <breno.barelli4@gmail.com>
2025-05-24 20:26:54 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex026.py
@@ -0,0 +1,6 @@
+# Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre: 
+#1.Quantas letras 'a'
+#2.Em que posição ela aparece a primeira vez
+#3.Em que posição ela aparece a última vez
+
+frase = input('Digite uma frase: ')
