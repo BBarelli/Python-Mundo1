0c1d3aefd2d73557bde4a72fa0a5f056e7846400
Breno Barelli <breno.barelli4@gmail.com>
2025-06-01 11:40:54 -0300

    Exercício Python - Curso em Vídeo

--- ex026.py
+++ ex026.py
@@ -1,14 +1,13 @@
-# Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre: 
-#1.Quantas letras 'a'
-#2.Em que posição ela aparece a primeira vez
-#3.Em que posição ela aparece a última vez
+# Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre:
 
-frase = input('Digite uma frase: ')
-print('Quantas vezes a letra (a) aparece: ') 
-frase.count('a')
 
-print('Em que posição ela aparece a primeira vez:')
-frase.count('a', 0:)
+# 1.Quantas letras 'a'
+frase = str(input('Digite uma frase: ')).upper()
+print('Quantas vezes a letra (a) aparece na frase: {}'.format(frase.count('A')))
 
-print('Em que posição ela aparece a última vez')
-frase.count('a': :)
+# 2.Em que posição ela aparece a primeira vez
+print('Em que posição ela aparece a primeira vez: {}'.format(frase.find('A')+1))
+
+# 3.Em que posição ela aparece a última vez
+# o rfind: Procure a partir do lado direito
+print('Em que posição ela aparece a última vez: {}'.format(frase.rfind('A')+1))
