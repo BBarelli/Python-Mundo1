a3399dbad5f8c3ef217ab02dffab772d364f9f29
Breno Barelli <157639296+BBarelli@users.noreply.github.com>
2025-06-02 07:53:31 -0300

    Atualizar o ex029.py

--- ex029.py
+++ ex029.py
@@ -3,8 +3,17 @@
 1. Se ele utrapassar 80km, mostre uma mensagem dizendo que ele foi multado.
 2. A multa vai custar R$ 7.00 por cada km acima do limite
 '''
-
 v = int(input('Digite a velocidade do carro: '))
+vmax = 80
+tx  = 7
+
 if v > 80:
-    print('Você foi multado!')
+    kmacima = v - vmax
+    multa = kmacima * 7
+    print('Você foi multado, o valor a ser pago é {:.2f}'.format(multa))
+else: 
+    print('Velocidade permitida.')
+
+    
+
 
