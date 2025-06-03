d26e00c77a690191d0d29ded92fee66071bf8161
BBarelli <breno.barelli4@gmail.com>
2025-06-03 17:35:45 -0300

    Exercício Python - Curso em Vídeo

--- ex033.py
+++ ex033.py
@@ -1,1 +1,11 @@
 #Exercício Python 33: Faça um programa que leia três números e mostre qual é o maior e qual é o menor
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int (input('Digite o terceiro número: '))
+
+if n1 > n2 and n1 > n3:
+    print('O maior número é {}'.format(n1))
+
+else:
+    n2 > n1 and n2 > n3
+    print('O maior número é ')
