a9675cd1195a7776017786b6688a4ed68341b958
BBarelli <breno.barelli4@gmail.com>
2025-06-03 14:59:18 -0300

    Exercício Python - Curso em Vídeo

--- ex032.py
+++ ex032.py
@@ -1,4 +1,9 @@
 #Exercício Python 32: Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
 
-ano = int(input('Digite um ano, e direi se é bissext ou não: '))
-b = ano
+ano = int(input('Digite um ano, e direi se é bissexto ou não: '))
+
+# A divisão por 4 com resto ZERO
+if ano % 4 == 0:
+    print('O ano é BISSEXTO.')
+else:
+    print('O ano não é BISSEXTO.')
