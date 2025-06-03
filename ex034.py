f3c423a6a64a0babeecf64967b67f0ff0ecc9005
BBarelli <breno.barelli4@gmail.com>
2025-06-03 14:27:32 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex034.py
@@ -0,0 +1,5 @@
+'''Exercício Python 34: Escreva um programa pergunte o salário de um funiconário e calcule o valor do seu aumento
+1. Para salários superiores a 1.250,00 calcule um aumento de 10%
+2. Para os inferiores ou iguais, o aumento é de 15%'''
+
+
