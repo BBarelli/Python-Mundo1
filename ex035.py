0f0d7f9cf5a08a2b48c88ee5cd93d86a1a114647
Breno Barelli <breno.barelli4@gmail.com>
2025-06-07 18:26:34 -0300

    Exercício Python - Curso em Vídeo

--- ex035.py
+++ ex035.py
@@ -1,1 +1,10 @@
-#Exercício Python 35: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo
+#Exercício Python 35: Desenvolva um programa que leia o comprimento de três retas
+#e diga ao usuário se elas podem ou não formar um triângulo
+r1 = int(input('Digite o valor da primeira reta: '))
+r2 = int(input('Digite o valor da segunda reta: '))
+r3 = int(input('Digite o valor da terceira reta: ')) 
+
+if (r1 + r2 > r3) and (r1 + r3 > r2) and (r2 + r3 > r1):
+    print('Sim é possível formar um triagulo.')
+else:
+    print('Com os comprimento informado não é possível formar um triagulo.')
