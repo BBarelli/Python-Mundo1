98da7e0077dff895300ff369ce67959c16ab10d2
Breno Barelli <breno.barelli4@gmail.com>
2025-06-08 00:24:53 -0300

    Exercício Python - Curso em Vídeo

--- ex031.py
+++ ex031.py
@@ -3,16 +3,12 @@ pergunte a distância de uma viagem em km
 Calcule o preço da passagem, cobrando R$0,50 por km para viagens até 200km
 e R$0,45 para viagens mais longa'''
 
-
 distancia = float(input('Quantos Km possui a viagem?:  '))
-limite = 200
-p1 = 0.50
-p2 = 0.45
 
-if distancia <= limite:
-    passagem = distancia * p1
+if distancia <= 200:
+    passagem = distancia * 0.50
     print('O valor da sua passagem é R$ {:.2f}'.format(passagem))
 
 else:
-    passagem = distancia * p2
+    passagem = distancia * 0.45
     print('O valor da sua passagem é R$ {:.2f}'.format(passagem))
