3f7c9beeab157ab21dfee530342e3994755ca510
Breno Barelli <breno.barelli4@gmail.com>
2025-06-08 00:04:52 -0300

    Exercício Python - Curso em Vídeo

--- ex028.py
+++ ex028.py
@@ -1,16 +1,24 @@
 ''' Exercicio em Python 28: Escreva um programa que faça o computador 'pensar' em um número entre 0 e 5
  e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador,
- o programa deverá escrever na tela se o usuário perdeu ou venceu.'''
+ o programa deverá escrever na tela se o usuário perdeu ou venceu.
 
 #Condição Simplificada
 n = int(input('De 0 a 5, em qual número estou pensando: '))
 print('Parabéns' if n == 3 else 'O computador venceu')
-
 
 # Condição composta
 n = int(input('De 0 a 5, em qual número estou pensando: '))
 if n == 3:
     print('Parabéns, você acertou!')
 else:
-    print('Que penas tente outra vez.')
+    print('Que penas tente outra vez.')'''
 
+from random import randint
+computador = randint(0, 5)
+jogador = int(input('Digite um numero: '))
+
+#condição
+if jogador == computador:
+    print('Parabéns você acertou!')
+else:
+    print('Que pena você ERROU!!! kkkk eu pensei no {} e não no {}'.format(computador, jogador))
