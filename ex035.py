acf515056381035f48bf476cac3fb6458385185e
BBarelli <breno.barelli4@gmail.com>
2025-06-09 12:06:49 -0300

    Exercício Python - Curso em Vídeo

--- ex035.py
+++ ex035.py
@@ -4,6 +4,6 @@ r2 = int(input('Digite o segundo comprimento: '))
 r3 = int(input('Digite o terceiro comprimento: '))
 #condição:
 if r1 < r2 + r3 and r1 < r3 + r2 and r3 < r1 + r2:
-    print('Sim é posível formar um triangulo com os valores fornecidos')
+    print('Sim, é possível formar um triângulo com os valores fornecidos.')
 else: 
-    print('Não é possível formar')
+    print('Não é possível formar um triângulo com os valores fornecidos.')
