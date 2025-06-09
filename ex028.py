9d2a4f46ef55090853156cc7037c178670042bc9
BBarelli <breno.barelli4@gmail.com>
2025-06-09 11:47:19 -0300

    Exercício Python - Curso em Vídeo

--- ex028.py
+++ ex028.py
@@ -1,8 +1,9 @@
-''' Exercicio em Python 28: Escreva um programa que faça o computador 'pensar' em um número entre 0 e 5 e peça para o usuário tentar descobrir
+''' Exercicio em Python 28: Escreva um programa que faça o computador 'pensar' em um número entre 0 e 5 e peça para o usuário tentar descobrirMore actions
 qual foi o número escolhido pelo computador, o programa deverá escrever na tela se o usuário perdeu ou venceu.'''
 
 #Condição Simplificada
 n = int(input('De 0 a 5, em qual número estou pensando: '))
+print('VENCEU' if n == 3 else 'PERDEU')
 print('Parabéns' if n == 3 else 'O computador venceu')
 
 
@@ -12,4 +13,3 @@ if n == 3:
     print('Parabéns, você acertou!')
 else:
     print('Que penas tente outra vez.')
-
