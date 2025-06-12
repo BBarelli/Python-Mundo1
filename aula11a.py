8209c78edabeaae3875f8b44d624366e95883f63
Breno Barelli <breno.barelli4@gmail.com>
2025-06-12 19:20:33 -0300

    Exercício Python - Curso em Vídeo

---
+++ aula11a.py
@@ -0,0 +1,30 @@
+'''
+Sempre que quiser incluir cores ao código incluir \033[style+text+back+m]
+Os stylos no python: 
+0: sem estilo
+1: negrito
+4: sublinado
+7: negative
+
+Cores no python:
+30: branco
+31: vermelho
+32: verde
+33: amarelo
+34: azul
+35: roxo
+36: azul claro
+37: cinza
+
+
+Backgroud:
+40:
+41:
+42:
+43:
+44:
+45:
+46:
+47:
+
+'''
