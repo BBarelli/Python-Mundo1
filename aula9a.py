811d91c9a85959db3a3295de540b8c91e2298e52
Breno Barelli <breno.barelli4@gmail.com>
2025-08-16 21:48:16 -0300

    aula9a

--- aula9a.py
+++ aula9a.py
@@ -1,103 +1,88 @@
- # FATIAMENTO DE STRING
+'''
+SOBRE STRING VEREMOS - (FATIAMENTO / ANALISE / DIVISAO / TRANSFORMAÇÃO)
 
- '''
- Exemplo: Curso em Video Python
-Totalizando 21 indice
+#FATIAMENTO DE STRING
+Exemplo: frase = 'Curso em Video Python - Totalizando 21 indice (letras)
 
-Pra mostrar apenas o indice, tem que informar:
-Frase[9] mostrara apenas a letra V
+Exibindo somente um indice:
+print((frase[9]))
 
-mas caso queira pegar o intervalo
-
-Frase [9:13]
+Caso queira pegar o intervalo de indices:
+print((frase [9:13]))
 
 ATENÇÃO!!! o valor informado no final do intervalo mostrará um número a menos do que foi informado
 No caso acima o que irá ficar visivel é do 9 a 12
 
 Outro exemplo é pulado as letras
-Frase[9:21:2]
-
-ele ira pular 2 casas até mostrar a próxima letra
+Frase[9:21:2] (inicio: 9, fim: 21, passo: 2)
+*ele ira pular 2 casas até mostrar a próxima letra
 
 Mas uma maneira
 Frase[:5]
-
-quando não informamos o valor de inicio, ele começa pelo indice zero
+*quando não informamos o valor de inicio, ele começa pelo indice zero
 
 Outra maneira
 Frase[9::3]
+*Inicia na casa 9, não tem indice final e vai pulando de 3 em 3 
+_______________________________________________________________________________
 
-Inicia na casa 9, não tem indice final e vai pulando de 3 em 3 
-
-'''
-
-# ANALISE
-'''
-*len(frase)      ///       len - significa Comprimento
-
+#ANALISE
+@length - sign. comprimento (No Python só 'len')
 ex: print(len(frase))
 21 caracteres
 
-Outra função
-
-*ex: frase.count('o')
+@'count' sign. (contar)
+*ex: print(frase.count('o'))
 Está pedindo pra contar quantas vezes a letra 'o' aparece
 
-uma outra maneira é fazer uma contagem + fatiamento
+uma outra maneira é fazer uma CONTAGEM + FATIAMENTO
 *ex: frase.count('o',0,13)
 
-*ex: frase.find('deo')    /// find - significa Encontrar
+@'find' sign. (encontrar)
+*ex: print(frase.find('deo'))
 
-*ex: IN Significa (em)
-
+@'in' sign. (entre)
 'Curso' in frase
 A resposta é True ou False, nesse caso é True
-'''
+
+frase = "Curso em Vídeo Python"
+resultado = 'Curso' in frase
+print(resultado)
+_____________________________________________________________________________
 
 #TRANSFORMAÇÃO
 
-'''
-*REPLACE - trocar, substituir
-
+@REPLACE - trocar, substituir
 ex: frase.replace('Python','Android')
 
-*UPPER - pra cima (metodo)
-
+@UPPER - pra cima (metodo)
 ex: frase.upper()
 
-*LOWER - pra baixo (metodo)
-
+@LOWER - pra baixo (metodo)
 ex: frase.lower()
 
-*CAPITALIZE() - em uma frase ele joga todos os caracteres pra minusculo, exceto a primeira letra que permanece Maiuscula 
-
+@CAPITALIZE() - em uma frase ele joga todos os caracteres pra minusculo, exceto a primeira letra que permanece Maiuscula 
 ex: frase.capitalize()
 
-*TITLE - Faz um papel parecido com o capitalize porém ele conta as palavras em uma frase e coloca em maisucula a primeira letra de cada palavra
-
+@TITLE - Faz um papel parecido com o capitalize porém ele conta as palavras em uma frase e coloca em maisucula a primeira letra de cada palavra
 ex: frase.title()
 
-* STRIP() - remove todos os espaçoes desnessários da string
-
+@STRIP() - remove todos os espaçoes desnessários da string
 ex: frase.stip()
-
 **RSTRIP - remove os espaços a direita da frase
 **LSTRIP - remove os espaços a esquerda da frase
-'''
+_____________________________________________________________________________
 
 #DIVISÃO
+{ESTUDAR!!: COMO FAZER UM SPLIT}
 
-'''
-ESTUDAR!!: COMO FAZER UM SPLIT
-
-*SPLIT() - vai divir uma string em uma lista onde cada elemento será separado por espaço
-
+@SPLIT() - vai divir uma string em uma lista onde cada elemento será separado por espaço
 ex: frase.split()
 
-*'-'JOIN - vai juntar uma string onde cada elemento era separado por espaço
+@'-'JOIN - vai juntar uma string onde cada elemento era separado por espaço
+ex: '-'join(frase) ('-' simbolo separador)
 
-ex: '-'join(frase)
+palavras = ["Python", "é", "incrível"]
+resultado = ' '.join(palavras)
+print(resultado)
 '''
-
-#BÔNUS
-#Para que o 'print'execute um texto grande é só colocar entre as triplas ('''  ''') abrindo e fechando  
